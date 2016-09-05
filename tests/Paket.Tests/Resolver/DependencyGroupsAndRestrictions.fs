28efb12bc79719da2498c7bab2267578cdc40274
Matthias Dittrich <Matthias.Dittrich@aitgmbh.de>
2016-09-06 01:03:57 +0200

    play around with test cases.

--- tests/Paket.Tests/Resolver/DependencyGroupsAndRestrictions.fs
+++ tests/Paket.Tests/Resolver/DependencyGroupsAndRestrictions.fs
@@ -7,11 +7,24 @@ open TestHelpers
 open Paket.Domain
 open Paket.PackageResolver
 
+let installModel (package:ResolvedPackage) files =
+    let nuspec =
+        Nuspec.Load("in-memory",
+            sprintf """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
+  <metadata>
+    <id>%s</id>
+    <version>%s</version>
+  </metadata>
+</package>""" (package.Name.GetCompareString()) (package.Version.AsString))
+    InstallModel.CreateFromLibs(package.Name, package.Version, package.Settings.FrameworkRestrictions |> Requirements.getRestrictionList, files, [], [], nuspec)
+
+
 let resolve graph updateMode (cfg : DependenciesFile) =
     let groups = [Constants.MainDependencyGroup, None ] |> Map.ofSeq
     cfg.Resolve(true,noSha1,VersionsFromGraphAsSeq graph,PackageDetailsFromGraph graph,groups,updateMode).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
-    
-let graph1 = 
+
+let graph1 =
   GraphOfNuspecs [
     """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
@@ -54,7 +67,7 @@ let graph1 =
 
 
 [<Test>]
-let ``should prefer all framework dependency to netstandard1.6``() = 
+let ``should prefer all framework dependency to netstandard1.6``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -65,8 +78,8 @@ nuget Chessie"""
         |> resolve graph1 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "FSharp.Core"] |> shouldEqual "4.0.0.1"
 
-   
-let graph2 = 
+
+let graph2 =
   GraphOfNuspecs [
     """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
@@ -103,9 +116,9 @@ let graph2 =
   </metadata>
 </package>"""
   ]
-  
+
 [<Test>][<Ignore("Currently failing")>]
-let ``should prefer netstandard1.3 dependency over all frameworks``() = 
+let ``should prefer netstandard1.3 dependency over all frameworks``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -116,8 +129,8 @@ nuget Chessie_test"""
         |> resolve graph2 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "FSharp.Core"] |> shouldEqual "4.0.1.7-alpha"
 
-     
-let graph3 = 
+
+let graph3 =
   GraphOfNuspecs [
     """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
@@ -137,7 +150,7 @@ let graph3 =
 
 
 [<Test>]
-let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net45)``() = 
+let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net45)``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net45
@@ -147,9 +160,9 @@ nuget Exceptionless"""
         DependenciesFile.FromCode(config)
         |> resolve graph3 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Exceptionless"] |> shouldEqual "4.0.1902"
-    
+
 [<Test>]
-let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net46)``() = 
+let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net46)``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -159,10 +172,10 @@ nuget Exceptionless"""
         DependenciesFile.FromCode(config)
         |> resolve graph3 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Exceptionless"] |> shouldEqual "4.0.1902"
-    
-    
+
+
 [<Test>]
-let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net463)``() = 
+let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net463)``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net463
@@ -173,8 +186,8 @@ nuget Exceptionless"""
         |> resolve graph3 UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Exceptionless"] |> shouldEqual "4.0.1902"
 
-   
-let graph4 = 
+
+let graph4 =
   GraphOfNuspecs [
     """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
@@ -195,13 +208,28 @@ let graph4 =
     <id>Npgsql</id>
     <version>3.1.4</version>
   </metadata>
+  <dependencies>
+    <group targetFramework=".NETFramework4.6">
+    </group>
+    <group targetFramework=".NETStandard1.3">
+      <dependency id="FrameworkPackage" version="[1.6.0, )" />
+    </group>
+  </dependencies>
+</package>
+    """
+    """<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+  <metadata>
+    <id>FrameworkPackage</id>
+    <version>1.6.0</version>
+  </metadata>
 </package>
     """
   ]
-  
-    
+
+
 [<Test>]
-let ``should use netstandard when no other choice exists``() = 
+let ``should use netstandard when no other choice exists``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -210,11 +238,45 @@ nuget Marten"""
     let resolved =
         DependenciesFile.FromCode(config)
         |> resolve graph4 UpdateMode.UpdateAll
-    getVersion resolved.[PackageName "Marten"] |> shouldEqual "0.9.12.563"
-    getVersion resolved.[PackageName "Npgsql"] |> shouldEqual "3.1.4"
+    let marten = resolved.[PackageName "Marten"]
+    getVersion marten |> shouldEqual "0.9.12.563"
+    let npgsql = resolved.[PackageName "Npgsql"]
+    getVersion npgsql |> shouldEqual "3.1.4"
+    let frameworkPackage = resolved.[PackageName "FrameworkPackage"]
+    getVersion npgsql |> shouldEqual "1.6.0"
 
-    
-let graph5 = 
+    // Note that we should still install the netstandard version of npgsql
+    let installNpgsql = installModel npgsql [ @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"; @"..\Npgsql\lib\net46\Npgsql.dll" ]
+
+    installNpgsql.GetLibReferences(SinglePlatform (FrameworkIdentifier.DotNetStandard DotNetStandardVersion.V1_3)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
+    installNpgsql.GetLibReferences(SinglePlatform (DotNetFramework FrameworkVersion.V4_6)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
+
+
+
+[<Test>]
+let ``should use netstandard when no other choice exists (all frameworks)``() =
+    let config = """
+source http://www.nuget.org/api/v2
+
+nuget Marten"""
+    let resolved =
+        DependenciesFile.FromCode(config)
+        |> resolve graph4 UpdateMode.UpdateAll
+
+    let marten = resolved.[PackageName "Marten"]
+    getVersion marten |> shouldEqual "0.9.12.563"
+    let npgsql = resolved.[PackageName "Npgsql"]
+    getVersion npgsql |> shouldEqual "3.1.4"
+    let frameworkPackage = resolved.[PackageName "FrameworkPackage"]
+    getVersion frameworkPackage |> shouldEqual "1.6.0"
+
+    // Note that we should still install the netstandard version of npgsql
+    let installNpgsql = installModel npgsql [ @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"; @"..\Npgsql\lib\net46\Npgsql.dll" ]
+
+    installNpgsql.GetLibReferences(SinglePlatform (FrameworkIdentifier.DotNetStandard DotNetStandardVersion.V1_3)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
+    installNpgsql.GetLibReferences(SinglePlatform (DotNetFramework FrameworkVersion.V4_6)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
+
+let graph5 =
   GraphOfNuspecs [
     """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
@@ -252,9 +314,9 @@ let graph5 =
 </package>
     """
   ]
-    
+
 [<Test>]
-let ``should use package even when not needed by one``() = 
+let ``should use package even when not needed by one``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -285,9 +347,9 @@ let graph6 =
   </metadata>
 </package>
     """ ]
-    
+
 [<Test>]
-let ``should properly delegate restriction when no global restriction is given``() = 
+let ``should properly delegate restriction when no global restriction is given``() =
     let config = """
 source http://www.nuget.org/api/v2
 
@@ -304,13 +366,118 @@ nuget Chessie"""
     getVersion fsharpCore |> shouldEqual "4.0.1.7-alpha"
     getVersion netStandard |> shouldEqual "1.6.0"
     // Don't install netstandard to net45
-    Requirements.isTargetMatchingRestrictions 
+    Requirements.isTargetMatchingRestrictions
       (Requirements.getRestrictionList netStandard.Settings.FrameworkRestrictions,
        (TargetProfile.SinglePlatform (FrameworkIdentifier.DotNetFramework FrameworkVersion.V4_5)))
       |> shouldEqual false
     // Don't install netstandard to net463
-    Requirements.isTargetMatchingRestrictions 
+    Requirements.isTargetMatchingRestrictions
       (Requirements.getRestrictionList netStandard.Settings.FrameworkRestrictions,
        (TargetProfile.SinglePlatform (FrameworkIdentifier.DotNetFramework FrameworkVersion.V4_6_3)))
       |> shouldEqual false
     // This also tests that "UnknownPackage" is not pulled unexpectedly (because this dependency is never relevant)
+
+
+
+let graph7 =
+  GraphOfNuspecs [
+    """<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
+  <metadata>
+    <id>Marten</id>
+    <version>0.9.12.563</version>
+    <dependencies>
+      <group targetFramework=".NETStandard1.3">
+        <dependency id="Npgsql" version="[3.1.4, )" />
+      </group>
+    </dependencies>
+  </metadata>
+</package>
+    """
+    """<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
+  <metadata>
+    <id>OtherPackage</id>
+    <version>1.0.0</version>
+    <dependencies>
+      <group targetFramework=".NETFramework4.6">
+        <dependency id="Npgsql" version="[3.1.4, )" />
+      </group>
+    </dependencies>
+  </metadata>
+</package>
+    """
+    """<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+  <metadata>
+    <id>Npgsql</id>
+    <version>3.1.4</version>
+  </metadata>
+  <dependencies>
+    <group targetFramework=".NETFramework4.6">
+    </group>
+    <group targetFramework=".NETStandard1.3">
+      <dependency id="FrameworkPackage" version="[1.6.0, )" />
+    </group>
+  </dependencies>
+</package>
+    """
+    """<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+  <metadata>
+    <id>FrameworkPackage</id>
+    <version>1.6.0</version>
+  </metadata>
+</package>
+    """
+  ]
+
+
+[<Test>]
+let ``netstandard when no other choice exists and other package uses higher``() =
+    let config = """
+source http://www.nuget.org/api/v2
+framework net46
+
+nuget Marten
+nuget OtherPackage"""
+    let resolved =
+        DependenciesFile.FromCode(config)
+        |> resolve graph7 UpdateMode.UpdateAll
+    let marten = resolved.[PackageName "Marten"]
+    getVersion marten |> shouldEqual "0.9.12.563"
+    let npgsql = resolved.[PackageName "Npgsql"]
+    getVersion npgsql |> shouldEqual "3.1.4"
+    let frameworkPackage = resolved.[PackageName "FrameworkPackage"]
+    getVersion npgsql |> shouldEqual "1.6.0"
+
+    // Note that we should still install the netstandard version of npgsql
+    let installNpgsql = installModel npgsql [ @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"; @"..\Npgsql\lib\net46\Npgsql.dll" ]
+
+    installNpgsql.GetLibReferences(SinglePlatform (FrameworkIdentifier.DotNetStandard DotNetStandardVersion.V1_3)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
+    installNpgsql.GetLibReferences(SinglePlatform (DotNetFramework FrameworkVersion.V4_6)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
+
+
+
+[<Test>]
+let ``netstandard when no other choice exists and other package uses higher (all frameworks)``() =
+    let config = """
+source http://www.nuget.org/api/v2
+
+nuget Marten"""
+    let resolved =
+        DependenciesFile.FromCode(config)
+        |> resolve graph7 UpdateMode.UpdateAll
+
+    let marten = resolved.[PackageName "Marten"]
+    getVersion marten |> shouldEqual "0.9.12.563"
+    let npgsql = resolved.[PackageName "Npgsql"]
+    getVersion npgsql |> shouldEqual "3.1.4"
+    let frameworkPackage = resolved.[PackageName "FrameworkPackage"]
+    getVersion frameworkPackage |> shouldEqual "1.6.0"
+
+    // Note that we should still install the netstandard version of npgsql
+    let installNpgsql = installModel npgsql [ @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"; @"..\Npgsql\lib\net46\Npgsql.dll" ]
+
+    installNpgsql.GetLibReferences(SinglePlatform (FrameworkIdentifier.DotNetStandard DotNetStandardVersion.V1_3)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
+    installNpgsql.GetLibReferences(SinglePlatform (DotNetFramework FrameworkVersion.V4_6)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
