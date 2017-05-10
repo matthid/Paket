183b790391e7ed6346974e6724c4a030646082b7
Matthias Dittrich <Matthias.Dittrich@aitgmbh.de>
2017-05-11 00:26:25 +0200

    some test updates

--- tests/Paket.Tests/Resolver/DependencyGroupsAndRestrictions.fs
+++ tests/Paket.Tests/Resolver/DependencyGroupsAndRestrictions.fs
@@ -24,9 +24,12 @@ let resolve graph updateMode (cfg : DependenciesFile) =
     let groups = [Constants.MainDependencyGroup, None ] |> Map.ofSeq
     cfg.Resolve(true,noSha1,VersionsFromGraphAsSeq graph,PackageDetailsFromGraph graph,groups,updateMode).[Constants.MainDependencyGroup].ResolvedPackages.GetModelOrFail()
 
-let graph1 =
-  GraphOfNuspecs [
-    """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+// As we restrict to net46 paket should detect that we don't need netstandard dependencies here.
+[<Test>]
+let ``should prefer all framework dependency to netstandard1.6``() =
+    let graph =
+      GraphOfNuspecs [
+        """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
   <metadata>
     <id>Chessie</id>
@@ -42,16 +45,16 @@ let graph1 =
     </dependencies>
   </metadata>
 </package>
-    """
-    """<?xml version="1.0"?>
+        """
+        """<?xml version="1.0"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
   <metadata>
     <id>FSharp.Core</id>
     <version>4.0.0.1</version>
   </metadata>
 </package>
-    """
-    """<?xml version="1.0" encoding="utf-8"?>
+        """
+        """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
   <metadata>
     <id>FSharp.Core</id>
@@ -63,11 +66,7 @@ let graph1 =
     </dependencies>
   </metadata>
 </package>"""
-  ]
-
-
-[<Test>]
-let ``should prefer all framework dependency to netstandard1.6``() =
+        ]
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -75,13 +74,15 @@ framework net46
 nuget Chessie"""
     let resolved =
         DependenciesFile.FromCode(config)
-        |> resolve graph1 UpdateMode.UpdateAll
+        |> resolve graph UpdateMode.UpdateAll
     getVersion resolved.[PackageName "FSharp.Core"] |> shouldEqual "4.0.0.1"
 
-
-let graph2 =
-  GraphOfNuspecs [
-    """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+/// netstandard1.3 is compatible with net46 -> Therefore it should be preferred to the fallback group.
+[<Test>]
+let ``should prefer netstandard1.3 dependency over all frameworks``() =
+    let graph =
+      GraphOfNuspecs [
+        """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
   <metadata>
     <id>Chessie_test</id>
@@ -96,16 +97,16 @@ let graph2 =
     </dependencies>
   </metadata>
 </package>
-    """
-    """<?xml version="1.0"?>
+        """
+        """<?xml version="1.0"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
   <metadata>
     <id>FSharp.Core</id>
     <version>4.0.0.1</version>
   </metadata>
 </package>
-    """
-    """<?xml version="1.0" encoding="utf-8"?>
+        """
+        """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
   <metadata>
     <id>FSharp.Core</id>
@@ -115,10 +116,9 @@ let graph2 =
     </dependencies>
   </metadata>
 </package>"""
-  ]
+      ]
 
-[<Test>][<Ignore("Currently failing")>]
-let ``should prefer netstandard1.3 dependency over all frameworks``() =
+
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -130,9 +130,12 @@ nuget Chessie_test"""
     getVersion resolved.[PackageName "FSharp.Core"] |> shouldEqual "4.0.1.7-alpha"
 
 
-let graph3 =
-  GraphOfNuspecs [
-    """<?xml version="1.0" encoding="utf-8"?>
+
+[<Test>]
+let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net45)``() =
+    let graph =
+      GraphOfNuspecs [
+        """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
   <metadata>
     <id>Exceptionless</id>
@@ -145,24 +148,39 @@ let graph3 =
     </dependencies>
   </metadata>
 </package>
-    """
-  ]
+        """
+      ]
 
-
-[<Test>]
-let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net45)``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net45
 
 nuget Exceptionless"""
+
     let resolved =
         DependenciesFile.FromCode(config)
-        |> resolve graph3 UpdateMode.UpdateAll
+        |> resolve graph UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Exceptionless"] |> shouldEqual "4.0.1902"
 
 [<Test>]
 let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net46)``() =
+    let graph =
+      GraphOfNuspecs [
+        """<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
+  <metadata>
+    <id>Exceptionless</id>
+    <version>4.0.1902</version>
+    <dependencies>
+      <group targetFramework=".NETFramework4.5" />
+      <group targetFramework=".NETStandard1.2">
+        <dependency id="MyNetStandardDummy" version="[1.6.0, )" />
+      </group>
+    </dependencies>
+  </metadata>
+</package>
+        """
+      ]
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -170,12 +188,29 @@ framework net46
 nuget Exceptionless"""
     let resolved =
         DependenciesFile.FromCode(config)
-        |> resolve graph3 UpdateMode.UpdateAll
+        |> resolve graph UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Exceptionless"] |> shouldEqual "4.0.1902"
 
 
 [<Test>]
 let ``should ignore netstandard deps when framework restrictions are enabled (prefer net45 for net463)``() =
+    let graph =
+      GraphOfNuspecs [
+        """<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
+  <metadata>
+    <id>Exceptionless</id>
+    <version>4.0.1902</version>
+    <dependencies>
+      <group targetFramework=".NETFramework4.5" />
+      <group targetFramework=".NETStandard1.2">
+        <dependency id="MyNetStandardDummy" version="[1.6.0, )" />
+      </group>
+    </dependencies>
+  </metadata>
+</package>
+        """
+      ]
     let config = """
 source http://www.nuget.org/api/v2
 framework net463
@@ -183,13 +218,17 @@ framework net463
 nuget Exceptionless"""
     let resolved =
         DependenciesFile.FromCode(config)
-        |> resolve graph3 UpdateMode.UpdateAll
+        |> resolve graph UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Exceptionless"] |> shouldEqual "4.0.1902"
 
 
-let graph4 =
-  GraphOfNuspecs [
-    """<?xml version="1.0" encoding="utf-8"?>
+
+[<Test>]
+let ``should use netstandard when no other choice exists``() =
+
+    let graph =
+      GraphOfNuspecs [
+        """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
   <metadata>
     <id>Marten</id>
@@ -201,8 +240,8 @@ let graph4 =
     </dependencies>
   </metadata>
 </package>
-    """
-    """<?xml version="1.0"?>
+        """
+        """<?xml version="1.0"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
   <metadata>
     <id>Npgsql</id>
@@ -216,20 +255,17 @@ let graph4 =
     </group>
   </dependencies>
 </package>
-    """
-    """<?xml version="1.0"?>
+        """
+        """<?xml version="1.0"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
   <metadata>
     <id>FrameworkPackage</id>
     <version>1.6.0</version>
   </metadata>
 </package>
-    """
-  ]
+        """
+      ]
 
-
-[<Test>]
-let ``should use netstandard when no other choice exists``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -237,7 +273,7 @@ framework net46
 nuget Marten"""
     let resolved =
         DependenciesFile.FromCode(config)
-        |> resolve graph4 UpdateMode.UpdateAll
+        |> resolve graph UpdateMode.UpdateAll
     let marten = resolved.[PackageName "Marten"]
     getVersion marten |> shouldEqual "0.9.12.563"
     let npgsql = resolved.[PackageName "Npgsql"]
@@ -255,13 +291,54 @@ nuget Marten"""
 
 [<Test>]
 let ``should use netstandard when no other choice exists (all frameworks)``() =
+
+    let graph =
+      GraphOfNuspecs [
+        """<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
+  <metadata>
+    <id>Marten</id>
+    <version>0.9.12.563</version>
+    <dependencies>
+      <group targetFramework=".NETStandard1.3">
+        <dependency id="Npgsql" version="[3.1.4, )" />
+      </group>
+    </dependencies>
+  </metadata>
+</package>
+        """
+        """<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+  <metadata>
+    <id>Npgsql</id>
+    <version>3.1.4</version>
+  </metadata>
+  <dependencies>
+    <group targetFramework=".NETFramework4.6">
+    </group>
+    <group targetFramework=".NETStandard1.3">
+      <dependency id="FrameworkPackage" version="[1.6.0, )" />
+    </group>
+  </dependencies>
+</package>
+        """
+        """<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+  <metadata>
+    <id>FrameworkPackage</id>
+    <version>1.6.0</version>
+  </metadata>
+</package>
+        """
+      ]
+
     let config = """
 source http://www.nuget.org/api/v2
 
 nuget Marten"""
     let resolved =
         DependenciesFile.FromCode(config)
-        |> resolve graph4 UpdateMode.UpdateAll
+        |> resolve graph UpdateMode.UpdateAll
 
     let marten = resolved.[PackageName "Marten"]
     getVersion marten |> shouldEqual "0.9.12.563"
@@ -276,9 +353,12 @@ nuget Marten"""
     installNpgsql.GetLibReferences(SinglePlatform (FrameworkIdentifier.DotNetStandard DotNetStandardVersion.V1_3)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
     installNpgsql.GetLibReferences(SinglePlatform (DotNetFramework FrameworkVersion.V4_6)) |> shouldContain @"..\Npgsql\lib\netstandard1.3\Npgsql.dll"
 
-let graph5 =
-  GraphOfNuspecs [
-    """<?xml version="1.0" encoding="utf-8"?>
+
+[<Test>]
+let ``should use package even when not needed by one``() =
+    let graph =
+      GraphOfNuspecs [
+        """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
   <metadata>
     <id>Marten</id>
@@ -290,16 +370,16 @@ let graph5 =
     </dependencies>
   </metadata>
 </package>
-    """
-    """<?xml version="1.0"?>
+        """
+        """<?xml version="1.0"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
   <metadata>
     <id>Npgsql</id>
     <version>3.1.4</version>
   </metadata>
 </package>
-    """
-    """<?xml version="1.0" encoding="utf-8"?>
+        """
+        """<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd">
   <metadata>
     <id>Exceptionless</id>
@@ -312,11 +392,9 @@ let graph5 =
     </dependencies>
   </metadata>
 </package>
-    """
-  ]
+        """
+      ]
 
-[<Test>]
-let ``should use package even when not needed by one``() =
     let config = """
 source http://www.nuget.org/api/v2
 framework net46
@@ -325,7 +403,7 @@ nuget Exceptionless
 nuget Marten"""
     let resolved =
         DependenciesFile.FromCode(config)
-        |> resolve graph5 UpdateMode.UpdateAll
+        |> resolve graph UpdateMode.UpdateAll
     getVersion resolved.[PackageName "Marten"] |> shouldEqual "0.9.12.563"
     getVersion resolved.[PackageName "Npgsql"] |> shouldEqual "3.1.4"
 
